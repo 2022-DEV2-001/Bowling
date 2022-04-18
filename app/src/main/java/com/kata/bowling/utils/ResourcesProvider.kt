905c82cc861d598b4b5ae1b2c7572c96b947528a
2022-DEV2-001 <2022dev2001@gmail.com>
2022-04-18 16:03:37 +0200

    Inject Application context to ResourcesProvider

--- app/src/main/java/com/kata/bowling/utils/ResourcesProvider.kt
+++ app/src/main/java/com/kata/bowling/utils/ResourcesProvider.kt
@@ -2,8 +2,14 @@ package com.kata.bowling.utils
 
 import android.content.Context
 import androidx.annotation.StringRes
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class ResourcesProvider(private val context: Context) {
+@Singleton
+class ResourcesProvider @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
     fun getString(@StringRes stringResId: Int): String {
         return context.getString(stringResId)
     }

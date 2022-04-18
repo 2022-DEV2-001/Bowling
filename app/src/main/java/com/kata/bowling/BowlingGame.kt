da47abe7194e536a7a4c986ec8987720f349cb83
2022-DEV2-001 <2022dev2001@gmail.com>
2022-04-18 09:52:38 +0200

    Code refactor, replaced magic numbers with constants

--- app/src/main/java/com/kata/bowling/BowlingGame.kt
+++ app/src/main/java/com/kata/bowling/BowlingGame.kt
@@ -6,12 +6,18 @@ class BowlingGame {
     private val frameList = arrayListOf<Frame>()
 
     init {
-        repeat((1..10).count()) {
-            frameList.add(Frame(-1, -1))
+        repeat((ONE..TEN).count()) {
+            frameList.add(Frame(INITIAL_VALUE, INITIAL_VALUE))
         }
     }
 
     fun getFrameList(): List<Frame> {
         return frameList
+    }
+
+    companion object {
+        const val INITIAL_VALUE = -1
+        const val ONE = 1
+        const val TEN = 10
     }
 }
